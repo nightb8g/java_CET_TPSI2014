8d056cf70fa29aa6d424981a129da42373162f2c
dj34ever <dj_34ever@hotmail.com>
2015-03-22 16:01:56 +0000

    chamado fx v1

--- src/bitmilhoes/AppStart.java
+++ src/bitmilhoes/AppStart.java
@@ -5,19 +5,27 @@
  */
 package bitmilhoes;
 
+import bitmilhoes.controller.PainelPrincipalController;
 import bitmilhoes.model.GestorAposta;
 import bitmilhoes.model.files.FicheiroEscritaBinario;
 import bitmilhoes.model.files.FicheiroEscritaTexto;
 import bitmilhoes.model.files.FicheiroLeituraBinario;
 import bitmilhoes.model.files.FicheiroLeituraTexto;
-import ficheiros.Pessoa;
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -27,11 +35,17 @@ public class AppStart extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        GestorAposta ga = new GestorAposta();
+        GestorAposta gestoraposta = new GestorAposta();
+        //testesConsola(gestoraposta);
+        testesFX(gestoraposta);
+      
+    }
+
+    private void testesConsola(GestorAposta ga) {
+        
         ga.novoApostador(212, (short) 1234, "OP", LocalDate.of(1980, Month.OCTOBER, 31), 30);
         ga.apostaAleatoria(212, (short) 1234);
-        
-        
+
         //escrever fcheiro
         FicheiroEscritaTexto fet = new FicheiroEscritaTexto(new File("Pessoas.txt"));
 
@@ -41,7 +55,7 @@ public class AppStart extends Application {
 
         //ler ficheiro
         FicheiroLeituraTexto flt = new FicheiroLeituraTexto(new File("Pessoas.txt"));
-        
+
         flt.abrir();
         do {
             String linha = flt.ler();
@@ -51,11 +65,10 @@ public class AppStart extends Application {
             System.out.println(linha);
         } while (true);
         flt.fechar();
-        
-        
+
         //escrever objecto
         FicheiroEscritaBinario feb = new FicheiroEscritaBinario(new File("Pessoas.bin"));
-       // System.out.println(ga);
+        // System.out.println(ga);
         feb.abrir();
         feb.escrever(ga.getApostadores());
         feb.fechar();
@@ -68,13 +81,24 @@ public class AppStart extends Application {
             if (linha == null) {
                 break;
             }
-           System.out.println(linha);
+            System.out.println(linha);
         } while (true);
         flb.fechar();
-        
-       
-        
         Platform.exit();
+    }
+
+    private Stage testesFX(GestorAposta gestoraposta) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/PainelPrincipal.fxml"));
+        Stage stage = new Stage(StageStyle.DECORATED);
+        try {
+            stage.setScene(new Scene((AnchorPane) loader.load()));
+        } catch (IOException ex) {
+            Logger.getLogger(AppStart.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        PainelPrincipalController controller = loader.<PainelPrincipalController>getController();
+        controller.initdata(gestoraposta,stage);
+        stage.show();
+        return stage;
     }
 
     /**
--- src/bitmilhoes/controller/PainelPrincipalController.java
+++ src/bitmilhoes/controller/PainelPrincipalController.java
@@ -5,9 +5,22 @@
  */
 package bitmilhoes.controller;
 
+import bitmilhoes.model.GestorAposta;
+import bitmilhoes.model.files.FicheiroEscritaBinario;
+import bitmilhoes.model.files.FicheiroLeituraBinario;
+import java.awt.Dialog;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +29,79 @@ import javafx.fxml.Initializable;
  */
 public class PainelPrincipalController implements Initializable {
 
+    private GestorAposta gestorAposta;
+    private Stage stage;
+    @FXML
+    private MenuItem menuitemImportar;
+    @FXML
+    private MenuItem menuitemExportar;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    public void initdata(GestorAposta gestorAposta, Stage stage)
+    {
+      this.gestorAposta=gestorAposta;
+      this.stage=stage;
+    }
+    public void setGestorAposta(GestorAposta gestorAposta) {
+        this.gestorAposta = gestorAposta;
+    }
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    @FXML
+    private void handleSairAction(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    private void handleImportar(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        
+         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("BIN", "*.bin"));
+        File file = fileChooser.showOpenDialog(stage);
+        //ler objecto
+        if (file == null) {
+            return;
+        }
+        FicheiroLeituraBinario<GestorAposta> flb = new FicheiroLeituraBinario(file);
+        flb.abrir();
+        do {
+            GestorAposta linha = flb.ler();
+            if (linha == null) {
+                break;
+            }
+            gestorAposta = linha;
+        } while (true);
+        flb.fechar();
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText(gestorAposta.toString());
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void handleExportar(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Resource File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("BIN", "*.bin"));
+        File file = fileChooser.showSaveDialog(stage);
+        //escrever objecto
+        FicheiroEscritaBinario feb = new FicheiroEscritaBinario(file);
+        if (file == null) {
+            return;
+        }
+        feb.abrir();
+        feb.escrever(gestorAposta);
+        feb.fechar();
+    }
+
+    
     
 }
